d9001e8c9da08acca121ec628ed332910a6e56bc
jensiriphone <jennetsiriphone@gmail.com>
2023-08-07 23:46:27 +1200

    format

--- diceroll.js
+++ diceroll.js
@@ -1,9 +1,9 @@
 function rollD6() {
-    const rollResult = Math.floor(Math.random() * 6 + 1);
-    document.getElementById("resultD6").innerText = rollResult;
-  }
-  
-  function rollD10() {
-    const rollResult = Math.floor(Math.random() * 10 + 1);
-    document.getElementById("resultD10").innerText = rollResult;
-  }
+  const rollResult = Math.floor(Math.random() * 6 + 1);
+  document.getElementById("resultD6").innerText = rollResult;
+}
+
+function rollD10() {
+  const rollResult = Math.floor(Math.random() * 10 + 1);
+  document.getElementById("resultD10").innerText = rollResult;
+}
